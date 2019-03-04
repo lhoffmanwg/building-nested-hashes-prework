8a379fc30faf55e7890c44008c4f477a32d1fcad
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-04 22:32:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => []
+      :hero_friends => [name: "Benolio", age: "17", attitude: "worried"]
    }, 
    :capulet => {
       :patriarch => {
@@ -30,7 +30,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [name: "Mercutio", age: "18", attitude: "hot-headed"]
    }
   }
 
