a23ca41646f97d24304593bf4abbe4ab931aa88b
Laura Hoffman <lhoffmanwg@gmail.com>
2019-03-04 22:23:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,15 +10,16 @@ def third_challenge
         age: "54"
         },
       :hero => {
-        name: "Romeo"
-        age: "15"
+        name: "Romeo",
+        age: "15",
         status: "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        name: "Lady Capulet",
+        age: "51"
         },
       :matriarch => {
         #your key/value pairs here
